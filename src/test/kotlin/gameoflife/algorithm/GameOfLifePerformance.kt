91de4f9ed19dc5ae00ad7585c960c367a80a03a1
Ming Jin <zejin@expedia.com>
2019-09-09 19:47:11 -0500

    Refactor: clean up

--- src/test/kotlin/gameoflife/algorithm/GameOfLifePerformance.kt
+++ src/test/kotlin/gameoflife/algorithm/GameOfLifePerformance.kt
@@ -10,20 +10,17 @@ class GameOfLifePerformance private constructor() {
 
         println("Started...")
         for (i in 0 until ITERATIONS) { gameOfLife = gameOfLife.tick() }
-        println(format(System.currentTimeMillis() - time))
+        println((System.currentTimeMillis() - time).format)
     }
 
     companion object {
-        private const val ITERATIONS = 500
-        private const val DEF_TEST_PATH = "src/main/resources/sidecar_gun.seed"
-
         @JvmStatic
         fun main(params: Array<String>) {
             GameOfLifePerformance().run(params)
         }
-
-        private fun format(time: Long): String {
-            return "Finished in %tM:%tS.%tL".format(time, time, time)
-        }
     }
 }
+private const val ITERATIONS = 500
+private const val DEF_TEST_PATH = "src/main/resources/sidecar_gun.seed"
+
+val Long.format: String get() = "Finished in %tM:%tS.%tL".format(this, this, this)
