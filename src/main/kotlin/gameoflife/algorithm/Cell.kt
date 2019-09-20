2bda25d6baa5d87e94f7e793c827a48e72776680
zejin <zejin@expediagroup.com>
2019-09-20 09:13:43 -0700

    Refactor: simplify

--- src/main/kotlin/gameoflife/algorithm/Cell.kt
+++ src/main/kotlin/gameoflife/algorithm/Cell.kt
@@ -3,13 +3,13 @@ package gameoflife.algorithm
 open class Cell(val x: Int, val y: Int) : Comparable<Cell> {
     private val string = toString(x, y)
 
-    val neighbours: List<Cell> get() = (y-1..y+1).map { getNeighboursByRow(it) }.flatten()
+    val neighbours: List<Cell> get() = (y-1..y+1).flatMap { getNeighboursByRow(it) }
 
     private fun getNeighboursByRow(row: Int) = (x-1..x+1).filter { !equals(it, row) }.map { Cell(it, row) }
 
     private fun equals(column: Int, row: Int) = x == column && y == row
 
-    override fun equals(other: Any?) = if (other is Cell) equals(other.x, other.y) else false
+    override fun equals(other: Any?) = other is Cell && equals(other.x, other.y)
 
     override fun hashCode() = string.hashCode()
 
