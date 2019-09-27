24c2d1f05845b61b0edf3014ca8cd68bd5d415d1
Ming Jin <zejin@expedia.com>
2019-09-26 19:56:21 -0500

    Refactor: replaced Long with Int

--- src/main/kotlin/gameoflife/algorithm/GameOfLife.kt
+++ src/main/kotlin/gameoflife/algorithm/GameOfLife.kt
@@ -3,8 +3,8 @@ package gameoflife.algorithm
 class GameOfLife(private val liveCellsMap: Map<String, Cell>) {
     val liveCells: List<Cell> get() = if (liveCellsMap.isNotEmpty()) liveCellsMap.values.toList() else emptyList()
 
-    private val isReproducible: (Long) -> Boolean = { n -> n == 3L }
-    private val isNextGeneration: (Long) -> Boolean = { n -> n in 2..3 }
+    private val isReproducible: (Int) -> Boolean = { n -> n == 3 }
+    private val isNextGeneration: (Int) -> Boolean = { n -> n in 2..3 }
 
     fun tick(): GameOfLife = GameOfLife(nextLiveCellsMap)
 
@@ -23,15 +23,14 @@ class GameOfLife(private val liveCellsMap: Map<String, Cell>) {
                 .distinct()
                 .filter { isInactive(it) }
 
-    private fun filteredCells(cells: List<Cell>, ifNotFiltered: (Long) -> Boolean): List<Cell> =
+    private fun filteredCells(cells: List<Cell>, ifNotFiltered: (Int) -> Boolean): List<Cell> =
             cells.filter { ifNotFiltered(countActiveNeighbours(it)) }
 
     fun isActive(x: Int, y: Int) = isActive(toString(x, y))
 
-    private fun isActive(key: String) = liveCellsMap[key] != null
+    private fun isActive(key: String) = liveCellsMap.containsKey(key)
 
     private fun isInactive(cell: Cell) = !isActive(cell.toString())
 
-    private fun countActiveNeighbours(cell: Cell): Long =
-            cell.neighbours.filter { isActive(it.toString()) }.count().toLong()
+    private fun countActiveNeighbours(cell: Cell): Int = cell.neighbours.filter { isActive(it.toString()) }.count()
 }
