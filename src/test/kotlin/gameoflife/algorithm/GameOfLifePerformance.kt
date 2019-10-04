569a683cd54a9113219ab843dd7f121e2fc92f91
zejin <zejin@expediagroup.com>
2019-10-04 03:57:22 -0500

    Refactor: simplify

--- src/test/kotlin/gameoflife/algorithm/GameOfLifePerformance.kt
+++ src/test/kotlin/gameoflife/algorithm/GameOfLifePerformance.kt
@@ -5,18 +5,15 @@ import gameoflife.helper.toTime
 import kotlin.system.measureTimeMillis
 
 object GameOfLifePerformance {
-    private fun run(params: Array<String>) {
+    @JvmStatic
+    fun main(params: Array<String>) {
         println("Testing $DEF_TEST_PATH in $ITERATIONS times...")
         var gameOfLife = params.buildGameOfLife(DEF_TEST_PATH)
 
         println("Started...")
-        measureTimeMillis { for (i in 1..ITERATIONS) gameOfLife = gameOfLife.tick() }
-                .apply { println("Finished in $toTime.") }
-    }
-
-    @JvmStatic
-    fun main(params: Array<String>) {
-        run(params)
+        measureTimeMillis {
+            for (i in (1..ITERATIONS)) { gameOfLife = gameOfLife.tick() }
+        }.apply { println("Finished in $toTime.") }
     }
 }
 
